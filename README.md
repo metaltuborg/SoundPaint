bad123e52fc1a266c64728d238cdd72d69884f03
metaltuborg <40725841+metaltuborg@users.noreply.github.com>
2020-11-22 21:23:20 +0000

    Functioning oscilloscope

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SoundPaint
-Draw something pretty with sound
+Draw something pretty with sound!
--- soundpaint.js
+++ soundpaint.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     acquireAudioStream($("#micStatus"), gotStream);
 });
 
@@ -12,7 +12,7 @@ function acquireAudioStream(statusDiv, onCapture) {
     // with getUserMedia as it would overwrite existing properties.
     // Here, we will just add the getUserMedia property if it's missing.
     if (navigator.mediaDevices.getUserMedia === undefined) {
-        navigator.mediaDevices.getUserMedia = function(constraints) {
+        navigator.mediaDevices.getUserMedia = function (constraints) {
             // First get ahold of the legacy getUserMedia, if present
             var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
@@ -23,13 +23,13 @@ function acquireAudioStream(statusDiv, onCapture) {
             }
 
             // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
-            return new Promise(function(resolve, reject) {
+            return new Promise(function (resolve, reject) {
                 getUserMedia.call(navigator, constraints, resolve, reject);
             });
         }
     }
 
-    navigator.mediaDevices.getUserMedia({audio: true}).then(successVal => {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(successVal => {
         statusDiv.text("Captured the mic!");
         onCapture(successVal);
     }, failureVal => {
@@ -38,11 +38,82 @@ function acquireAudioStream(statusDiv, onCapture) {
 }
 
 function gotStream(stream) {
-    
+    var painter = new SoundPaint({});
+    painter.source = painter.context.createMediaStreamSource(stream);
+    painter.source.connect(painter.analyser);
+    painter.init();
 }
 
 var AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
 
-function SoundPaint(settings) {
-    this.context = new AudioContext();
+class SoundPaint {
+    constructor(settings) {
+        // Resolution of the spectrum analyser. Must be a power of 2.
+        // The frequency bin count is half of this resolution.
+        this.fftsize = typeof settings.fftsize === "undefined" ? Math.pow(2, 12) : settings.fftsize;
+        this.smoothingTimeConstant = typeof settings.smoothingTimeConstant === "undefined" ? 0 : settings.smoothingTimeConstant;
+
+        this.binCountLog = $("#binCount");
+        this.deltaLog = $("#delta");
+        this.freqDataLog = $("#freqData");
+        this.canvas = $("#canvas")[0];
+        this.ctx = this.canvas.getContext('2d');
+
+        this.context = new AudioContext();
+        this.analyser = this.context.createAnalyser();
+        this.analyser.smoothingTimeConstant = this.smoothingTimeConstant;
+        this.analyser.fftSize = this.fftsize;
+        this.binCount = this.analyser.frequencyBinCount;
+        this.freqDomain = new Uint8Array(this.binCount);
+
+        this.lastStep = undefined;
+    }
+
+    init() {
+        this.binCountLog.text(this.binCount);
+        this.render();
+    }
+
+    render(timestamp) {
+        if (timestamp != undefined && this.lastStep != undefined) {
+            this.deltaLog.text(timestamp - this.lastStep);
+        }
+        this.lastStep = timestamp;
+
+        this.analyser.getByteFrequencyData(this.freqDomain);
+        this.freqDataLog.text(this.freqDomain.join(" "));
+
+        this.renderOscilloscope(this.freqDomain);
+
+        window.requestAnimationFrame(this.render.bind(this));
+    }
+
+    renderOscilloscope(data) {
+        this.ctx.fillStyle = "rgb(200, 200, 200)";
+        this.ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        this.ctx.lineWidth = 2;
+        this.ctx.strokeStyle = "rgb(0, 0, 0)";
+
+        this.ctx.beginPath();
+
+        var sliceWidth = canvas.width * 1.0 / this.binCount;
+        var x = 0;
+
+        for (var i = 0; i < this.binCount; i++) {
+            var v = data[i] / 128.0;
+            var y = v * canvas.height / 2;
+
+            if (i === 0) {
+                this.ctx.moveTo(x, y);
+            } else {
+                this.ctx.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+        }
+
+        this.ctx.lineTo(canvas.width, canvas.height / 2);
+        this.ctx.stroke();
+    }
 }
